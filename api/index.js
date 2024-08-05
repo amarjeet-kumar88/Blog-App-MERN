b1ff6b314857635d9331220c4533a8dac6dfc2a8
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2024-08-05 16:41:45 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -14,7 +14,8 @@ mongoose
   });
 
 const app = express();
+const port = process.env.PORT || 4000
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
