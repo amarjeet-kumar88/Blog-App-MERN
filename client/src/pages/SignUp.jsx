b2be3303fe48928c2f7824e20843244148406765
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2024-08-06 16:28:13 +0530

    create and add the footer component

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -41,7 +41,7 @@ export default function SignUp() {
 
 
   return (
-    <div className='min-h-screen mt-20'>
+    <div className='min-h-screen pt-20 bg-sky-100'>
     <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
       {/* left */}
       <div className='flex-1'>
