af8e85b36fd37cbca3fba0b3caf3bb3bf7f5ba6b
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2024-08-06 17:47:30 +0530

    complete signin page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Signin from "./pages/Signin"
+import SignIn from "./pages/Signin"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
@@ -15,7 +15,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/about" element={ <About />} />
-        <Route path="/sign-in" element={ <Signin />} />
+        <Route path="/sign-in" element={ <SignIn />} />
         <Route path="/sign-up" element={ <SignUp />} />
         <Route path="/dashboard" element={ <Dashboard />} />
         <Route path="/projects" element={ <Projects />} />
