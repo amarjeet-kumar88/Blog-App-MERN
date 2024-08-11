c1a87dc741b8e8b362b67bc4c55ab37e45097730
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2024-08-11 15:18:12 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -33,6 +33,7 @@ export const updateUser = async (req, res, next) => {
           errorHandler(400, 'Username can only contain letters and numbers')
         );
       }
+    }
       try {
         const updatedUser = await User.findByIdAndUpdate(
           req.params.userId,
@@ -51,5 +52,4 @@ export const updateUser = async (req, res, next) => {
       } catch (error) {
         next(error);
       }
-    }
   };
