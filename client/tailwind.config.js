a77a75ff2b182dc2a19d232b56529afcb388f6b2
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2024-08-22 19:17:59 +0530

    show user posts inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,8 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-    // require('tailwind-scrollbar'),
-  ],
+  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
 };
